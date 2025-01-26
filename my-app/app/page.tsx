1317b792433ebaf6344c904a17370c747598798e
[Evin Rasho] <erasho2@huskers.unl.edu>
2025-01-25 22:08:39 -0600

    another push

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className='flex flex-col items-center justify-center max-h-screen h-3/6 py-2 overflow-hidden'>
             <div className='flex flex-col items-center w-full flex-1 px-10 text-center'>
                 <Image priority src='/logo-big.svg' alt='Evin Rasho' width={600} height={500} className='h-auto w-auto object-cover' />
-                <h1 className='text-xl md:text-4xl font-bold'>Welcome to Evin&apos;s Webpage!</h1>
+                <h1 className='text-xl md:text-4xl font-bold font-serif'>Welcome to Evin&apos;s Webpage!</h1>
             </div>
         </div>
     );
